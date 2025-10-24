fe161962d072a3c81c63d26ba08e8936a78ccb41
Jj87-huy <haoallain964@gmail.com>
2025-10-24 12:34:49 +0700

    Update dark-screen.lua

--- dark-screen.lua
+++ dark-screen.lua
@@ -1,14 +1,23 @@
--- Giả sử bạn có một ScreenGui với Button
-local button = script.Parent
+-- Lấy tham chiếu đến 2 nút
+local gui = script.Parent
+local button1 = gui:WaitForChild("Button1")
+local button2 = gui:WaitForChild("Button2")
 
-button.MouseButton1Click:Connect(function()
-	print("Chạy code mới!")
-	
-	-- Ví dụ chạy một hàm mới
-	loadstring([[
-		print("Hello từ script mới!")
-		for i = 1, 5 do
-			print("Dòng "..i)
-		end
-	]])()
+-- Khi nhấn Button1
+button1.MouseButton1Click:Connect(function()
+	print("Chạy code của nút 1!")
+
+	-- Ví dụ: di chuyển nhân vật
+	local player = game.Players.LocalPlayer
+	local character = player.Character or player.CharacterAdded:Wait()
+	character:MoveTo(Vector3.new(0, 10, 0))
+end)
+
+-- Khi nhấn Button2
+button2.MouseButton1Click:Connect(function()
+	print("Chạy code của nút 2!")
+
+	-- Ví dụ: đổi màu trời
+	game.Lighting.Ambient = Color3.new(1, 0.5, 0.5)
+	game.Lighting.Brightness = 3
 end)
